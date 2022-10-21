9b208a75afa9962e37d4ad069d77f5f03d6d40bf
jujinhyeong <mhn03148@naver.com>
2022-10-22 02:38:44 +0900

    성적평균

--- average.py
+++ average.py
@@ -1,1 +1,6 @@
-a,b = map(int,input().split)
+a,b = map(int,input().split())
+grade = list(map(int, input().split()))
+for i in range(b):
+    start, end = map(int, input().split())
+    print(f'{sum(grade[start-1:end])/(end - start + 1) :.2f}')
+print(grade)
